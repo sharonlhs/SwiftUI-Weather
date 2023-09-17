24a4ab2ba178d0f4d13d43a03885641057da5749
sharonlhs <121072143+sharonlhs@users.noreply.github.com>
2023-09-17 10:09:45 +0800

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Lim Hor Shun on 17/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
